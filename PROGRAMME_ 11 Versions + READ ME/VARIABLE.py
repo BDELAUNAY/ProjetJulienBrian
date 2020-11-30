792a07fdbc9b0035da93eae0d83b3c65b9717173
JULIEN <jcotisson@gmail.com>
2020-11-30 14:58:54 +0100

    Create VARIABLE.py

---
+++ PROGRAMME_ 11 Versions + READ ME/VARIABLE.py
@@ -0,0 +1,13 @@
+def indice_variable (mot):
+    if mot == "Bruit":
+        return 0
+    elif mot == "température":
+        return 1
+    elif mot == "Humidité":
+        return 2
+    elif mot == "luminosité":
+        return 3
+    elif mot == "C02":
+        return 4 
+    elif mot == "Indice humidex":
+        return 5
